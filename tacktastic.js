08637ecc44728b3fe37687049eceb02f24cfb914
Joseph Ladino <drtnt25@gmail.com>
2019-12-06 10:25:32 -0500

    currently remaking the engine for 3d tic tac toe

--- tacktastic.js
+++ tacktastic.js
@@ -1,16 +1,30 @@
-var req = null, cubeSize = 300;
+var req = null, cubeScale = 200, objString = "", viewLevel = 0, dragStartTime = 0, dragEndTime = 0, dragStartCoords = {x: 0, y: 0}, oldcubeRotation = [0, 0, 0], cubeRotation = [0, 0, 0], doIt = false;
 
 function loop() {
-  render(objects, ctx, (ctx.canvas.width/2), ctx.cavas.height/2);
-  
+
+  if(oldcubeRotation[0] < cubeRotation[0]) oldcubeRotation[0]++;
+  else if(oldcubeRotation[0] > cubeRotation[0]) oldcubeRotation[0]--;
+  if(oldcubeRotation[1] < cubeRotation[1]) oldcubeRotation[1]++;
+  else if(oldcubeRotation[1] > cubeRotation[1]) oldcubeRotation[1]--;
+  if(oldcubeRotation[2] < cubeRotation[2]) oldcubeRotation[2]++;
+  else if(oldcubeRotation[2] > cubeRotation[2]) oldcubeRotation[2]--;
+  //
+  // objects[0].orientation[0] = (cubeRotation[0]%180 == 0) ? oldcubeRotation[1] : -oldcubeRotation[1];
+  // objects[0].orientation[0] = oldcubeRotation[1];
+  // if(cubeRotation[1]%90 == 0 && Math.abs(cubeRotation[1]) != 180) objects[0].orientation[2] = oldcubeRotation[0];
+  // else objects[0].orientation[1] = oldcubeRotation[0];
+
+  if(doIt) rotate(camera, cameraBase, oldcubeRotation[0], oldcubeRotation[1], oldcubeRotation[2]);
+
+  render(objects, ctx, (ctx.canvas.width/2), ctx.canvas.height/2);
   req = requestAnimationFrame(loop);
 }
 
 function stop() {
   objects = [];
-  fillColor = "";
-  camera = new Vert3(0, 0, 600);
-  e = new Vert3((ctx.canvas.width/2), (ctx.canvas.height/2), 200);
+  camera = new Vert3(0, 0, 1500);
+  cameraBase = new Vert3(0, 0, 1500);
+  e = new Vert3((ctx.canvas.width/2), (ctx.canvas.height/2), 300);
   tx = 0;
   ty = 0;
   camera.orientation = [0, 0, 0];
@@ -21,20 +35,56 @@ function stop() {
 function togglePause() {
   if(req === null) req = requestAnimationFrame(loop);
   else {cancelAnimationFrame(req); req = null;}
-  
 }
 
-function start() {
-  stop();
-  var parent = {vertices: [new Vert3(0, 0, 0)], base:[new Vert3(0, 0, 0)],  subObjs: [], size: size/2};
-  
-  for(var i = 0, j = 0, k = 0, sub; i < 3; i++) {
-    for(j = 0; j < 3; j++) {
-      for(k = 0; k < 3; k++) {
-        sub = {polygons:[]};
-      }
-    }
+function triangleArea(...points) {
+  var p1 = points[0], p2 = points[1], p3 = points[2];
+  return Math.abs((p1.x*(p2.y-p3.y) + p2.x*(p3.y-p1.y) + p3.x*(p1.y-p2.y))/2);
+}
+
+function whichCube(crd) {
+  var all = [];
+  for(var i = 0, poly, area; i < projected.length; i++) {
+    poly = projected[i].polygon;
+    area = triangleArea(poly[0], poly[1], poly[2])+triangleArea(poly[0], poly[3], poly[2]);
+    if(area == triangleArea(poly[0], poly[1], crd)+triangleArea(poly[1], poly[2], crd)+triangleArea(poly[2], poly[3], crd)+triangleArea(poly[3], poly[0], crd)) all.push(projected[i].cube);
   }
-  
+
+  if(all.length > 0) return all[viewLevel];
+  else return false;
+}
+
+function manageClick(e) {
+  e.preventDefault();
+  if(e.button == 0 && e.type == "mousedown") {
+    var cur = whichCube(new Vert2(e.x, e.y));
+    cur.fillColor = "rgba(255,0,0,0.75)";
+  } else if(e.button == 2) {
+    if(e.type == "mousedown") {
+      dragStartTime = (new Date()).getTime()/100;
+      dragStartCoords = new Vert2(e.x, e.y);
+    } else {
+      dragEndTime = (new Date()).getTime()/100;
+
+      if(Math.abs(e.x-dragStartCoords.x) >= 50) cubeRotation[0] += (e.x-dragStartCoords.x > 0) ? -90 : 90;
+      if(Math.abs(e.y-dragStartCoords.y) >= 50) cubeRotation[1] += (e.y-dragStartCoords.y > 0) ? 90 : -90;
+      cubeRotation[0] %= 360;
+      cubeRotation[1] %= 360;
+    }
+    return false;
+  }
+}
+
+async function start() {
+  var waiting = await fetch("./base.obj");
+  objString = await waiting.text();
+  stop();
+  loadObj(cubeScale, objString);
   req = requestAnimationFrame(loop);
 }
+
+start();
+
+ctx.canvas.addEventListener("mousedown", manageClick);
+ctx.canvas.addEventListener("mouseup", manageClick);
+ctx.canvas.addEventListener("contextmenu", (e)=>{e.preventDefault()});
--- v1.js
+++ v1.js
@@ -6,22 +6,21 @@
 */
 
 var ctx = document.getElementById("mycan").getContext("2d");
-    
+
     // fixes width and height
     ctx.canvas.width = ctx.canvas.offsetWidth;
     ctx.canvas.height = ctx.canvas.offsetHeight;
-    
+
 var objects = [],
-    fillColor = "",
-    camera = new Vert3(0, 0, 600),
+    camera = new Vert3(0, 0, 1500),
+    cameraBase = new Vert3(0, 0, 1500),
     e = new Vert3((ctx.canvas.width/2), (ctx.canvas.height/2), 200),
-    inter,
+    projected = [],
     tx = 0,
     ty = 0;
     camera.orientation = [0, 0, 0];
-    
-    
-function Vert3 (...coords) {
+
+function Vert3(...coords) {
   this.x = parseFloat(coords[0]);
   this.y = parseFloat(coords[1]);
   this.z = parseFloat(coords[2]);
@@ -37,7 +36,7 @@ function rotateX(vert, angle) {
   var base = {y:vert.y*1, z:vert.z*1}, rad = (360 + angle) * Math.PI/180, cos = Math.cos(rad), sin = Math.sin(rad);
   vert.y =  base.y*cos - base.z*sin;
   vert.z =  base.y*sin + base.z*cos;
-  
+
   vert.orientation[0] = angle;
 }
 
@@ -45,7 +44,7 @@ function rotateY(vert, angle) {
   var base = {x:vert.x*1, z:vert.z*1}, rad = (360 + angle) * Math.PI/180, cos = Math.cos(rad), sin = Math.sin(rad);
   vert.x = base.z*sin + base.x*cos;
   vert.z = -base.x*sin + base.z*cos;
-  
+
   vert.orientation[1] = angle;
 }
 
@@ -53,143 +52,166 @@ function rotateZ(vert, angle) {
   var base = {x:vert.x*1, y:vert.y*1}, rad = (360 + angle) * Math.PI/180, cos = Math.cos(rad), sin = Math.sin(rad);
   vert.x = base.x*cos - base.y*sin;
   vert.y = base.x*sin + base.y*cos;
-  
+
   vert.orientation[2] = angle;
 }
 
-function rotate(vert, angleX, angleY, angleZ) {
-  if(angleY) rotateY(vert, vert, angleY);
-  if(angleZ) rotateZ(vert, vert, angleZ);
-  if(angleX) rotateX(vert, vert, angleX);
+function rotate(vert, base, angleX, angleY, angleZ) {
+  var noRef = new Vert3(base.x, base.y, base.z);
+
+  if(angleY) rotateY(noRef, angleY);
+  if(angleZ) rotateZ(noRef, angleZ);
+  if(angleX) rotateX(noRef, angleX);
+
+  vert.x = noRef.x;
+  vert.y = noRef.y;
+  vert.z = noRef.z;
+  vert.orientation = noRef.orientation;
 }
 
 function rotateTo(parentObj, angleX, angleY, angleZ) {
-  
+
   var noRef = [];
   for(var i = 0, temp; i < parentObj.base.length; i++) {
     temp  = parentObj.vertices[i];
     noRef[i] = new Vert3(temp.x, temp.y, temp.z);
-    
+
     if(angleY) rotateY(noRef[i], angleY);
     if(angleZ) rotateZ(noRef[i], angleZ);
     if(angleX) rotateX(noRef[i], angleX);
-    
-    temp = parentObj.vertices[i];
+
     temp.x = noRef[i].x;
     temp.y = noRef[i].y;
     temp.z = noRef[i].z;
   }
 }
 
-function backcull(p) {
-  
-  var N = new Vert3(0, 0, 0),
-      T = {x: p[0].x-camera.x, y: p[0].y-camera.y, z: p[0].z-camera.z},
-      vec1 = {x: p[1].x-p[0].x, y: p[1].y-p[0].y, z: p[1].z-p[0].z},
-      vec2 = {x: p[2].x-p[0].x, y: p[2].y-p[0].y, z: p[2].z-p[0].z};
-  
-  // cross product
-  N.x = (vec1.y * vec2.z) - (vec1.z * vec2.y);
-  N.y = (vec1.z * vec2.x) - (vec1.x * vec2.z);
-  N.z = (vec1.x * vec2.y) - (vec1.y * vec2.x);
-  
-  // if the dot product of T and N is greater than 0, discard this polygon
-  if((T.x * N.x) + (T.y * N.y) + (T.z * N.z) >= 0) return true;
-  else return false;
-}
+function cameraTransform(obj) {
 
-function project(obj, avgZ) {
-  
-  // distance between vertice and camera (per dimension)
+  // vector between vertice and camera (per dimension)
   var x = obj.x - camera.x,
       y = obj.y - camera.y,
       z = obj.z - camera.z,
-      
+
       // sines of camera orientation
       sx = Math.sin(camera.orientation[0]*Math.PI/180),
       sy = Math.sin(camera.orientation[1]*Math.PI/180),
       sz = Math.sin(camera.orientation[2]*Math.PI/180),
-      
+
       // cosines of camera orientation
       cx = Math.cos(camera.orientation[0]*Math.PI/180),
       cy = Math.cos(camera.orientation[1]*Math.PI/180),
       cz = Math.cos(camera.orientation[2]*Math.PI/180),
-      
+
       // camera transformation
       dx = (cy * ((sz*y) + (cz*x))) - (sy*z),
       dy = (sx * ((cy*z) + (sy*((sz*y) + (cz*x))))) + (cx*((cz*y) - (sz*x))),
-      dz = (cx * ((cy*z) + (sy*((sz*y) + (cz*x))))) - (sx*((cz*y) - (sz*x))),
-      
+      dz = (cx * ((cy*z) + (sy*((sz*y) + (cz*x))))) - (sx*((cz*y) - (sz*x)));
+
+      this.x = dx; this.y = dy; this.z = dz;
+}
+
+function project(obj) {
       // perspective projection
-      outX = ((e.z/dz)*dx) + e.x,
-      outY = ((e.z/dz)*dy) + e.y;
-  
+  var d = new cameraTransform(obj),
+      outX = ((e.z/d.z)*d.x) + e.x,
+      outY = ((e.z/d.z)*d.y) + e.y;
+
+  // return new Vert2((e.z/obj.z)*obj.x+e.x, (e.z/obj.z)*obj.y+e.x);
   return new Vert2(outX, outY);
 }
 
+function projectBulk(arr) {
+  var out = [];
+  for(var i = 0; i < arr.length; i++) { out[i] = project(arr[i]) }
+  return out;
+}
+
+function backcull(p) {
+  var N = new Vert3(0, 0, 0),
+      T = {x: p[0].x-camera.x, y: p[0].y-camera.y, z: p[0].z-camera.z},
+      vec1 = {x: p[1].x-p[0].x, y: p[1].y-p[0].y, z: p[1].z-p[0].z},
+      vec2 = {x: p[2].x-p[0].x, y: p[2].y-p[0].y, z: p[2].z-p[0].z};
+
+  // cross product
+  N.x = (vec1.y * vec2.z) - (vec1.z * vec2.y);
+  N.y = (vec1.z * vec2.x) - (vec1.x * vec2.z);
+  N.z = (vec1.x * vec2.y) - (vec1.y * vec2.x);
+
+  // if the dot product of T and N is greater than 0, the polygon is facing away from the camera
+  if((T.x * N.x) + (T.y * N.y) + (T.z * N.z) >= 0) return true;
+  else return false;
+}
+
 function render(objects, canvas, dx, dy) {
-  
+
   // Clears the canvas and sets the colors for stroke and fill
   canvas.clearRect(0, 0, 2*dx, 2*dy);
-  
+
   canvas.strokeStyle = "black";
-  canvas.fillStyle = fillColor;
   e.x = dx + tx;
   e.y = dy + ty;
-  
+
   // loops through parent objects
   for(var i = 0; i < objects.length; i++) {
-    
+
     for(var kawaii = 0, t, t2; kawaii < objects[i].vertices.length; kawaii++) {
       t = objects[i].base[kawaii];
       t2 = objects[i].vertices[kawaii];
-      t2.x = t.x * objects[i].size;
-      t2.y = t.y * objects[i].size;
-      t2.z = t.z * objects[i].size;
+      t2.x = t.x * objects[i].scale;
+      t2.y = t.y * objects[i].scale;
+      t2.z = t.z * objects[i].scale;
     }
-    
+
     rotateTo(objects[i], ...objects[i].orientation);
-    
+
     var zBuf = [];
-    
+    projected = [];
     // loops through current parent's subobjects
     for(var j = 0; j < objects[i].subObjs.length; j++) {
-      
+
       // loops through polygons in the current subobject
       for(var k = 0, tempval; k < (objects[i].subObjs[j].polygons).length; k++) {
-        var zObj = {zval: 0, polygon: [] }, leastbeast = 0;
-        
+        var zObj = {zval: 0, polygon: [], fillColor: "", cube: {}}, leastbeast = Infinity;
+
         // loops through vertices in the current polygon
         for(var l = 0; l < objects[i].subObjs[j].polygons[k].length; l++) {
           tempval = e.z - objects[i].subObjs[j].polygons[k][l].z;
-          if(tempval > leastbeast) leastbeast = tempval;
+          if(tempval < leastbeast) leastbeast = tempval;
         }
+
         zObj.zval = leastbeast;
         zObj.polygon = objects[i].subObjs[j].polygons[k];
-        if(!backcull(zObj.polygon)) zBuf.push(zObj);
+        zObj.fillColor = objects[i].subObjs[j].fillColor;
+        zObj.cube = objects[i].subObjs[j];
+        zBuf.push(zObj);
+        if(!backcull(zObj.polygon)) {
+          projected.push({zval:leastbeast,polygon:projectBulk(objects[i].subObjs[j].polygons[k]),cube:objects[i].subObjs[j]});
+        }
       }
     }
-    
+
     zBuf.sort((a, b)=>{return (a.zval > b.zval) ? -1 : ((a.zval < b.zval) ? 1 : 0)});
-  
+    projected.sort((a, b)=>{return (a.zval > b.zval) ? -1 : ((a.zval < b.zval) ? 1 : 0)});
+
     for(var zCount = 0; zCount < zBuf.length; zCount++) {
       var verts = zBuf[zCount].polygon,
-        
       P = project(verts[0]);
-      
+
+      canvas.fillStyle = zBuf[zCount].fillColor;
       canvas.beginPath();
       canvas.moveTo(P.x, P.y);
-        
+
       // loops through vertices (excluding first one)
       for(var lego = 1; lego < verts.length; lego++) {
         P = project(verts[lego]);
         canvas.lineTo(P.x, P.y);
       }
-      
+
       // ends path and draws
       canvas.closePath();
-      canvas.stroke();
       canvas.fill();
+      canvas.stroke();
     }
   }
 }
@@ -198,25 +220,29 @@ function ParentObj3D(obj) {
   this.base = obj.base;
   this.vertices = obj.vertices;
   this.subObjs = obj.subObjs;
-  this.size = obj.size;
+  this.scale = obj.scale;
   this.orientation = [0, 0, 0];
 }
 
 function SubObj3D(obj) {
   this.polygons = obj.polygons;
+  this.fillColor = "rgba(0,0,0,0)";
+  this.x = obj.x;
+  this.y = obj.y;
+  this.z = obj.z;
 }
 
-function loadObj(size, fileString) {
+function loadObj(scale, fileString) {
   var subObjs = (fileString.indexOf("o ") > -1) ? fileString.split("o ") : fileString.split("g ");
-  var parent = {vertices: [new Vert3(0, 0, 0)], base:[new Vert3(0, 0, 0)],  subObjs: [], size: size/2};
-  
+  var parent = {vertices: [new Vert3(0, 0, 0)], base:[new Vert3(0, 0, 0)],  subObjs: [], scale: scale};
+
   // loops through all subobjects
   for(var i = 0; i < subObjs.length; i++) {
-    
+
     // splits subobject string into array
     subObjs[i] = subObjs[i].split("\n");
     var out = {polygons: [], parent: {}}, cur, temp;
-    
+
     // loops through all the items (lines) in the array
     for(var j = 0; j < subObjs[i].length; j++) {
       cur = subObjs[i][j], temp = cur.split(" ");
@@ -227,7 +253,7 @@ function loadObj(size, fileString) {
         parent.base.push(new Vert3(temp[1+jkrowling], temp[2+jkrowling], temp[3+jkrowling]));
       }
     }
-    
+
     // loops through all the lines again
     for(j = 0; j < subObjs[i].length; j++) {
       cur = subObjs[i][j], temp = cur.split(" ");
@@ -241,7 +267,7 @@ function loadObj(size, fileString) {
       }
     }
     // pushes subobject to parent object
-    out.parent = parent;
+    // out.parent = parent;
     if(out.polygons.length > 0) parent.subObjs.push(new SubObj3D(out));
   }
   // pushes parent object to the objects array
@@ -249,4 +275,4 @@ function loadObj(size, fileString) {
   render([objects[0]], ctx, ctx.canvas.width/2, ctx.canvas.height/2);
 }
 
-window.addEventListener("resize", ()=>{ctx.canvas.width = ctx.canvas.offsetWidth; ctx.canvas.height = ctx.canvas.offsetHeight; render([objects[0]], ctx, ctx.canvas.width/2, ctx.canvas.height/2)});
+window.addEventListener("resize", ()=>{ctx.canvas.width = ctx.canvas.offsetWidth; ctx.canvas.height = ctx.canvas.offsetHeight; render(objects, ctx, ctx.canvas.width/2, ctx.canvas.height/2)});
